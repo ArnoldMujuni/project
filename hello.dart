254bd120333170b0359f255809b59ac4ebeb1477
Arnold <amutuhairearnold5@gmail.com>
2023-09-07 12:06:35 +0300

    hello.dart

---
+++ hello.dart
@@ -0,0 +1,24 @@
+int age = 43;
+var time = 567;
+bool answer =true;
+final int a = 43;
+final int t =567;
+String school = "must";
+List <String> classes = ["one", "two","three","four","five"];
+Map<int,String> week= {
+  1 :"monday",
+  2:"tuesday",
+  3:"wednesday",
+  4:"thursday",
+  5:"friday",
+  6:"saturday",
+  7:"sunday"
+};
+void main(){
+  
+  print("hello world");
+  print(school);
+  print("classes are $classes" );
+  print(classes[0]);
+  print(week[3]);
+}
